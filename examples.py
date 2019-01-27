962330d228738159170bf5ed80385734a2c4baef
Clay Elmore <ClayElmore@Clays-MacBook-Pro.local>
2019-01-27 14:14:10 -0500

    jupyter addition

--- examples.py
+++ examples.py
@@ -1,6 +1,6 @@
 # lets create a simple data structure
 data = [0,5,1,-2,7]
-print('Data before function =',data)
+# print('Data before function =',data)
 
 # now create a function to perform an action on the data
 # in this case we will just add a certain number
@@ -11,5 +11,26 @@ def add_to_data(data,num):
 
 # now lets manipulate the data
 data = add_to_data(data,3)
-print('Data after function =',data)
+# print('Data after function =',data)
+
+# now lets create a class to do the same thing as before
+class data:
+
+	# initalize the data
+	def __init__(self):
+		self.vector = [0,5,1,-2,7]
+
+	# create a function to perform the same task
+	def add_to_vec(self,num):
+		for ind,val in enumerate(self.vector):
+			self.vector[ind] += num
+
+# create the object
+obj = data()
+print('Vector upon initalization: ',obj.vector)
+
+# manipulate the data
+obj.add_to_vec(3)
+print('Vector after manipulatation: ',obj.vector)
+
 
--- oop.py
+++ oop.py
@@ -1,1 +1,11 @@
-### basic python file
+# basic python class
+class data:
+
+	# initalize the data
+	def __init__(self):
+		self.vector = [0,5,1,-2,7]
+
+	# create a function to perform the same task
+	def add_to_vec(self,num):
+		for ind,val in enumerate(self.vector):
+			self.vector[ind] += num
